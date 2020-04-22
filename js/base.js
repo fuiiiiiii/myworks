e7cf9bd24d02301ddea064d78adde3f4c4de8d73
foch <fu.967@qq.com>
2020-04-22 18:14:09 +0800

    首页调整

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".ham").click(function(){
-    $(".menubor").animate({height:'400px'},"slow");
+    $(".menubor").animate({height:'500px'},"slow");
   });
   $(".closebor").click(function(){
     $(".menubor").animate({height:'0px'},"slow");

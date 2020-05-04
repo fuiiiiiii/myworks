2f7d350a5ff64f6a9d8ef7e0b64994402b2a67ea
foch <fu.967@qq.com>
2020-05-04 23:06:05 +0800

    首页跟联系我页面修改

--- js/base.js
+++ js/base.js
@@ -1,8 +1,39 @@
-$(document).ready(function(){
-  $(".ham").click(function(){
-    $(".menubor").animate({height:'500px'},"slow");
+$(document).ready(function () {
+  $(".ham").click(function () {
+    $(".menubor").animate({
+      height: '500px'
+    }, "slow");
   });
-  $(".closebor").click(function(){
-    $(".menubor").animate({height:'0px'},"slow");
+  $(".closebor").click(function () {
+    $(".menubor").animate({
+      height: '0px'
+    }, "slow");
   })
+
+  let href = location.href
+  if (href.indexOf('home') >= 0) {
+    typing()
+
+    $('.bounce').click(function () {
+      $('html,body').animate({
+        scrollTop: $('.prodlist').offset().top
+      }, 200); //距离body顶部为0，scrollTop
+
+    })
+  }
+
+
 });
+
+let homeChar = 'Made In San Francisco'
+var i = 0
+
+function typing() {
+  var divTyping = document.getElementsByClassName('addfont')[0]
+  if (i <= homeChar.length) {
+    divTyping.innerHTML = homeChar.slice(0, i++) + ""
+    setTimeout("typing()", 150)
+  } else {
+    divTyping.innerHTML = homeChar
+  }
+}
